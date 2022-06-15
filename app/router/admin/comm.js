7a8e8c78ef2f600f0652fb6cd669e9909b35f787
bohecola <bohecola@outlook.com>
2022-06-15 13:02:11 +0800

    update

--- app/router/admin/comm.js
+++ app/router/admin/comm.js
@@ -4,10 +4,10 @@ const commController = require('../../controller/admin/comm');
 const router = new Router();
 
 router
+  .prefix('/comm')
   .post('/personUpdate', commController.personUpdate)
   .get('/person', commController.person)
   .get('/permmenu', commController.permmenu)
-  .get('/person', commController.person)
   .get('/upload', commController.upload);
 
 module.exports = router;
